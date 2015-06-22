fd35b5e6d24521544c16f2b3a194280be3745508
Mukesh Patil <mukesh.patil@citruspay.com>
2015-06-22 14:35:08 +0530

    - Git repo pushed

---
+++ SampleApp/CitrusPay-iOS-SDK-Sample-App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CitrusPay-iOS-SDK-Sample-App
+//
+//  Created by Mukesh Patil on 22/06/15.
+//  Copyright (c) 2015 CitrusPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
